54fc9a20bea4ef525d16491c611a0795c2448b35
jul2264 <julianstevenj@gmail.com>
2026-03-24 09:04:51 +0530

    Base Application Setup

---
+++ main.java
@@ -0,0 +1,7 @@
+public static void main (String[] args){
+    System.out.println("======================================");
+    System.out.println("======Train Consist Managemnt App ====");
+    System.out.println("======================================");
+    List<String> trainConsist = new ArrayList<>();
+    System.out.println("Train Initialized Sucessfully....");      
+}
